00e5a9f1dac2b79f2bfef6f8074705daad54bc6b
SteelStarling <thancock0319@gmail.com>
2021-09-04 20:02:05 -0700

    Initial Commit

---
+++ core/src/com/projectattica/game/Being.java
@@ -0,0 +1,4 @@
+package com.projectattica.game;
+
+public class Being {
+}
---
+++ desktop/src/com/projectattica/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.projectattica.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.projectattica.game.ProjectAttica;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ProjectAttica(), config);
+	}
+}
